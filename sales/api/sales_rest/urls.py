7018f20bd03a2031f55efe7400625c25b0eaf602
JakTac <jake.p.tippit@gmail.com>
2022-12-06 22:28:38 -0600

    backend sorta working mostly

---
+++ sales/api/sales_rest/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import api_list_sales_person, api_show_sales_person, api_list_customers, api_show_customer, api_list_sales, api_show_sale
+
+urlpatterns = [
+    path("sales_person/", api_list_sales_person, name="api_create_sales_person"),
+    path("sales_person/<int:pk>/", api_show_sales_person, name="api_show_sales_person"),
+    path("customer/", api_list_customers, name="api_create_customer"),
+    path("customer/<int:pk>/", api_show_customer, name="api_show_customer"),
+    path("sales/", api_list_sales, name="api_create_sales"),
+    path("sales_person/<int:salesperson_vo_id>/sales/", api_list_sales, name="api_list_sales"),
+    path("sales/<int:pk>/", api_show_sale, name="api_show_sale"),
+]
--- sales/poll/poller.py
+++ sales/poll/poller.py
@@ -9,18 +9,27 @@ sys.path.append("")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sales_project.settings")
 django.setup()
 
-# Import models from sales_rest, here.
-# from sales_rest.models import Something
+from sales_rest.models import AutoMobileVO
+
+def get_automobiles():
+    response = requests.get("http://inventory-api:8000/api/automobiles/")
+    content = json.loads(response.content)
+    for automobile in content["autos"]:
+        AutoMobileVO.objects.update_or_create(
+            import_href=automobile["href"],
+            vin=automobile['vin'],
+            defaults={"vin": automobile["vin"]},
+        )
+        print(AutoMobileVO.objects.all().values())
 
 def poll():
     while True:
         print('Sales poller polling for data')
         try:
-            # Write your polling logic, here
-            pass
+            get_automobiles()
         except Exception as e:
             print(e, file=sys.stderr)
-        time.sleep(60)
+        time.sleep(5)
 
 
 if __name__ == "__main__":
